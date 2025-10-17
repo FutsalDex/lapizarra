2aa71b1fe91a20bf4842a4a97f678e0950ec34b7
FutsalDex <futsaldex@gmail.com>
2025-10-17 09:53:16 +0000

    Re-init shadcn y agregar components para fix build

--- src/.next/static/chunks/src_app_crear-sesion_page_tsx_ea644c45._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/src_20b03136._.js",
-  "static/chunks/node_modules_49af81a7._.js"
-],
-    source: "dynamic"
-});
--- src/.next/static/chunks/src_app_gestion-equipos_page_tsx_ea644c45._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/src_55293f71._.js",
-  "static/chunks/node_modules_21b6db81._.js"
-],
-    source: "dynamic"
-});
